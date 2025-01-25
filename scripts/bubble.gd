2313a4e9f186307eef5a2469a0c343ac8cee2c63
Logan MacDougall <loganmac3000@hotmail.com>
2025-01-25 10:10:32 -0400

    Bubble works with player

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,15 +1,23 @@
+class_name Bubble
 extends Area2D
 
 const BLOW_RATE = 0.5
 const POP_SIZE = 1.25
 
 @onready var sprite = %AnimatedSprite2D
+@onready var ColShape0: CollisionShape2D = %ColShape0
+@onready var ColShape1: CollisionShape2D = %ColShape1
+@onready var ColShape2: CollisionShape2D = %ColShape2
+@onready var ColShape3: CollisionShape2D = %ColShape3
+@onready var ColShape4: CollisionShape2D = %ColShape4
+@onready var ColShape5: CollisionShape2D = %ColShape5
+
 var bubble_size = 0.0
 var blowing_bubble = true
 
 var bubble_frame : int : 
 	get():
-		return min(floor(bubble_size * 6), 5)
+		return min(floor(bubble_size * 5), 5)
 
 var grow_animation_name : String : 
 	get():
@@ -19,16 +27,45 @@ var pop_animation_name : String :
 	get():
 		return "pop_" + str(bubble_frame)	
 
+func _ready() -> void:
+	activate_correct_collider()
+	visible = false
+
 func _process(delta: float):
 	if blowing_bubble:
 		bubble_size += BLOW_RATE * delta
 		sprite.play(grow_animation_name)
+		activate_correct_collider()
 		
-func blow_bubble(value: bool):
-	blowing_bubble = value
+	if bubble_size > 0:
+		visible = true
+		
+	if bubble_size >= POP_SIZE:
+		pop_bubble()
 	
+func activate_correct_collider():
+	var CollisionList = [ColShape0, ColShape1, ColShape2, ColShape3, ColShape4, ColShape5]
+	
+	var current_bubble_frame = bubble_frame if bubble_size > 0 else -1
+	for i in range(CollisionList.size()):
+		var col: CollisionShape2D = CollisionList[i]
+		var col_disabled = i != current_bubble_frame
+		var needs_change = col.disabled != col_disabled
+		if needs_change:
+			col.set_deferred("disabled", col_disabled)
+
 func pop_bubble():
-	blowing_bubble = false
 	sprite.play(pop_animation_name)
+	blowing_bubble = false
 	bubble_size = 0
-	
+	activate_correct_collider()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		return
+		
+	pop_bubble()
+
+
+func _on_animation_finished() -> void:
+	visible = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
-const BLOW_RATE = 0.5
 const FLUTTER_POWER = 100.0
 const MIN_BUBBLE_POWER = 500
 const MAX_BUBBLE_POWER = 900
@@ -12,14 +11,21 @@ const ANI_FLUTTER_RIGHT = "going_right"
 const ANI_BLOW_STANDING = "ready_to_blow"
 const ANI_STANDING = "standing_still"
 
+@onready var bubble : Bubble = %Bubble
 @onready var sprite : AnimatedSprite2D = %AnimatedSprite2D
 
 var previously_on_floor = true
 var just_landed_on_floor : bool : 
 	get():
 		return is_on_floor() and not previously_on_floor
-var blowing_bubble = false
-var bubble_size = 0.0
+var blowing_bubble: bool:
+	get():
+		return bubble.blowing_bubble
+	set(v):
+		bubble.blowing_bubble = v
+var bubble_size: float:
+	get():
+		return bubble.bubble_size
 
 var base_gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 var gravity: float:
@@ -38,6 +44,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		handle_fluttering(delta)
 		
+		
 	velocity.y = min(velocity.y + gravity * delta, gravity / 2)
 	previously_on_floor = is_on_floor()
 	
@@ -53,10 +60,7 @@ func handle_bubble_blowing(delta: float):
 		blowing_bubble = false
 	
 	if just_landed_on_floor:
-		bubble_size = 0
-		
-	if blowing_bubble:
-		bubble_size += BLOW_RATE * delta
+		bubble.pop_bubble()
 		
 	if bubble_size > 0:
 		gravity = base_gravity - lerpf(MIN_BUBBLE_POWER, MAX_BUBBLE_POWER, min(bubble_size, 1))
@@ -64,12 +68,16 @@ func handle_bubble_blowing(delta: float):
 		gravity = base_gravity
 
 func handle_jumping():
+	var just_pressed_jump = Input.is_action_just_pressed("Jump")
 	var just_released_jump = Input.is_action_just_released("Jump")
 	var on_floor = is_on_floor()
 	
 	if just_released_jump and on_floor:
 		print(gravity)
 		velocity.y = JUMP_VELOCITY
+	elif just_pressed_jump and not blowing_bubble and not on_floor and bubble_size > 0:
+		bubble.pop_bubble()
+	
 	
 func handle_fluttering(delta: float):	
 	var direction = Input.get_axis("Left", "Right")
