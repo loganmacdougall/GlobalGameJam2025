aa5b0376270be4ab6bbd4104d180b6ac497db636
Logan MacDougall <loganmac3000@hotmail.com>
2025-01-25 09:04:38 -0400

    Added colliders to bubble

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,4 +1,34 @@
 extends Area2D
 
-func get_grow_animation(size: float):
-	const division =  1 / 6
+const BLOW_RATE = 0.5
+const POP_SIZE = 1.25
+
+@onready var sprite = %AnimatedSprite2D
+var bubble_size = 0.0
+var blowing_bubble = true
+
+var bubble_frame : int : 
+	get():
+		return min(floor(bubble_size * 6), 5)
+
+var grow_animation_name : String : 
+	get():
+		return "grow_" + str(bubble_frame)
+
+var pop_animation_name : String : 
+	get():
+		return "pop_" + str(bubble_frame)	
+
+func _process(delta: float):
+	if blowing_bubble:
+		bubble_size += BLOW_RATE * delta
+		sprite.play(grow_animation_name)
+		
+func blow_bubble(value: bool):
+	blowing_bubble = value
+	
+func pop_bubble():
+	blowing_bubble = false
+	sprite.play(pop_animation_name)
+	bubble_size = 0
+	
